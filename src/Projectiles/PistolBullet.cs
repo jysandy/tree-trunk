fe128641d7e7b646e9552d3ac06a5444c5192e22
Sandilya Jandhyala <jysandilya@gmail.com>
2024-05-23 13:33:24 +0530

    Add collision animation to ShotgunBullet

--- src/Projectiles/PistolBullet.cs
+++ src/Projectiles/PistolBullet.cs
@@ -1,8 +1,7 @@
 using Godot;
 using System;
-using TreeTrunk;
 
-public partial class ShotgunBullet : Bullet
+public partial class PistolBullet : Bullet
 {
 	private AnimatedSprite2D BulletAnimation
 	{
@@ -17,9 +16,6 @@ public partial class ShotgunBullet : Bullet
 	public override void _Ready()
 	{
 		base._Ready();
-		// TODO: Remove shotgun bullets after they travel a certain distance,
-		// rather than after a certain amount of time
-		this.RunLater(0.2f, () => QueueFree());
 
 		CollisionAnimation.AnimationFinished += () => QueueFree();
 
