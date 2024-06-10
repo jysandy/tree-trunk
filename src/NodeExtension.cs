84cda66ecd4f316601b8da44a011c5d42749bd89
Sandilya Jandhyala <jysandilya@gmail.com>
2024-06-10 14:26:25 +0530

    Use async Task instead of async void

--- src/NodeExtension.cs
+++ src/NodeExtension.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using Godot;
 
 namespace TreeTrunk
@@ -7,7 +8,7 @@ namespace TreeTrunk
     {
 
         // The node needs to be added to the scene tree for this to work
-        public static async void RunLater(this Node node, double timeSeconds, Action f)
+        public static async Task RunLater(this Node node, double timeSeconds, Action f)
         {
             await node.ToSignal(node.GetTree().CreateTimer(timeSeconds), SceneTreeTimer.SignalName.Timeout);
             f();
