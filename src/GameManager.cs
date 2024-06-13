27f852ba8497a49584d6280c0070e1f7a32fcc50
Sandilya Jandhyala <jysandilya@gmail.com>
2024-06-13 16:33:54 +0530

    Some null checks in the GameManager

--- src/GameManager.cs
+++ src/GameManager.cs
@@ -5,59 +5,78 @@ using TreeTrunk;
 
 public partial class GameManager : Node
 {
-    public Root RootNode { get { return GetTree().Root.GetNode<Root>("Root"); } }
-    public Node CurrentScene { get { return RootNode.CurrentScene; } }
+	public Root RootNode
+	{
+		get
+		{
+			if (!IsInsideTree()) return null;
 
-    // Physics layer names mapped to layer numbers
-    private Dictionary<string, int> _physicsLayers = new Dictionary<string, int>();
+			var foo = GetTree().Root;
+			if (foo != null)
+			{
+				return foo.GetNode<Root>("Root");
+			}
+			else
+			{
+				return null;
+			}
+		}
+	}
+	public Node CurrentScene { get { return RootNode.CurrentScene; } }
 
-    private PlayerState _playerState;
+	// Physics layer names mapped to layer numbers
+	private Dictionary<string, int> _physicsLayers = new Dictionary<string, int>();
 
-    public PlayerState PlayerState { get { return _playerState; } }
+	private PlayerState _playerState;
 
-    public PlayerCharacter Player
-    { get { return ((ILevelRoot)CurrentScene).Player; } }
+	public PlayerState PlayerState { get { return _playerState; } }
 
-    public bool NavigationMapReady
-    { get { return ((ILevelRoot)CurrentScene).NavigationMapReady; } }
+	public PlayerCharacter Player
+	{ get { return ((ILevelRoot)CurrentScene).Player; } }
 
-    public void AddToCurrentScene(Node node)
-    {
-        CurrentScene.AddChild(node);
-    }
+	public bool NavigationMapReady
+	{ get { return ((ILevelRoot)CurrentScene).NavigationMapReady; } }
 
-    public override void _Ready()
-    {
-        base._Ready();
-        _playerState = new PlayerState();
-        AddChild(_playerState);
+	public void AddToCurrentScene(Node node)
+	{
+		CurrentScene.AddChild(node);
+	}
 
-        PlayerState.CurrentAmmoChanged += RootNode.HUD.OnPlayerCharacterCurrentAmmoChanged;
+	public override void _Ready()
+	{
+		base._Ready();
+		_playerState = new PlayerState();
+		AddChild(_playerState);
 
-        for (int i = 1; i <= 32; i++)
-        {
-            string layerName = (string)ProjectSettings.GetSetting("layer_names/2d_physics/layer_" + i);
-            if (!string.IsNullOrEmpty(layerName))
-            {
-                _physicsLayers[layerName] = i;
-            }
-        }
-    }
+		if (RootNode != null && RootNode.HUD != null)
+		{
+			PlayerState.CurrentAmmoChanged += RootNode.HUD.OnPlayerCharacterCurrentAmmoChanged;
+		}
 
-    public uint BuildPhysicsLayerMask(string layerName)
-    {
-        int layerNumber;
-        if (!_physicsLayers.TryGetValue(layerName, out layerNumber))
-        {
-            return 0;
-        }
+		for (int i = 1; i <= 32; i++)
+		{
+			string layerName = (string)ProjectSettings.GetSetting("layer_names/2d_physics/layer_" + i);
+			if (!string.IsNullOrEmpty(layerName))
+			{
+				_physicsLayers[layerName] = i;
+			}
+		}
+	}
 
-        return (uint)1 << (layerNumber - 1);
-    }
+	public uint BuildPhysicsLayerMask(string layerName)
+	{
+		int layerNumber;
+		if (!_physicsLayers.TryGetValue(layerName, out layerNumber))
+		{
+			return 0;
+		}
 
-    public uint BuildPhysicsLayerMask(IEnumerable<string> layerNames)
-    {
-        return layerNames.Select(BuildPhysicsLayerMask)
-            .Aggregate((mask, next) => mask | next);
-    }
+		return (uint)1 << (layerNumber - 1);
+	}
+
+	public uint BuildPhysicsLayerMask(IEnumerable<string> layerNames)
+	{
+		return layerNames.Select(BuildPhysicsLayerMask)
+			.Aggregate((mask, next) => mask | next);
+	}
 }
